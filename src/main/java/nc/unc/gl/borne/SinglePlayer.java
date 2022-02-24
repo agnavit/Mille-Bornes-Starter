04a2a68d7f15078c16ce8dae41ab709f296d8df5
Gnavit Anthony <68042249+agnavit@users.noreply.github.com>
2022-02-24 15:56:48 +1300

    Nettoyage

--- src/main/java/nc/unc/gl/borne/SinglePlayer.java
+++ src/main/java/nc/unc/gl/borne/SinglePlayer.java
@@ -6,11 +6,11 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.router.Route;
 
 @Route
-@Tag("multi-player")
-public class MultiPlayer extends HtmlContainer {
+@Tag("single-player")
+public class SinglePlayer extends HtmlContainer {
 	
-	public MultiPlayer() {
-		H1 title = new H1("hello word Multi-player");
+	public SinglePlayer() {
+		H1 title = new H1("hello word Solo");
 		add(title);
 	}
 }
