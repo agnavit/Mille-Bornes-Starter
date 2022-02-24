c18b5f1c12b5731253bcc5198648d3378c28a64d
Gnavit Anthony <68042249+agnavit@users.noreply.github.com>
2022-02-24 16:05:00 +1300

    correction

--- src/main/java/nc/unc/gl/borne/SinglePlayer.java
+++ src/main/java/nc/unc/gl/borne/SinglePlayer.java
@@ -5,10 +5,10 @@ import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.router.Route;
 
-@Route
+@Route("single-player")
 @Tag("single-player")
 public class SinglePlayer extends HtmlContainer {
-	
+
 	public SinglePlayer() {
 		H1 title = new H1("hello word Solo");
 		add(title);
