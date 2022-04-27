a70f7f584ce29cddef6922cb4f3c04b198b3c150
Gnavit Anthony <68042249+agnavit@users.noreply.github.com>
2022-04-28 09:19:36 +1200

    Ajout des règles

--- src/main/java/nc/unc/gl/borne/views/MainView.java
+++ src/main/java/nc/unc/gl/borne/views/MainView.java
@@ -4,6 +4,12 @@ import java.util.concurrent.atomic.AtomicReference;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.component.HtmlContainer;
@@ -29,18 +35,25 @@ public class MainView extends HtmlContainer {
         titleImage.addClassName("main-view-title");
         title.add(titleImage);
 
+        Dialog rules = new Dialog();
+        rules.getElement().setAttribute("aria-label", "Les règles");
+
+        VerticalLayout dialogLayout = showRules(rules);
+        rules.add(dialogLayout);
+
         // Création des sous-titres
-        H2 play = new H2("play");
-        play.addClassName("main-view-subtitle");
-        H2 rules = new H2("rules");
-        rules.addClassName("main-view-subtitle");
+        H2 playSubtitle = new H2("play");
+        playSubtitle.addClassName("main-view-subtitle");
+        H2 rulesSubtitle = new H2("rules");
+        rulesSubtitle.addClassName("main-view-subtitle");
 
         // Création des événements des sous-titres
-        play.addClickListener(click -> UI.getCurrent().navigate("party"));
-        rules.addClickListener(click -> Notification.show("Les règles ne sont pas encore disponible"));
+        playSubtitle.addClickListener(click -> UI.getCurrent().navigate("party"));
+        rulesSubtitle.addClickListener(click -> rules.open());
+
 
         // Les éléments sont ajoutés au conteneur
-        container.add(title, play, rules);
+        container.add(title, playSubtitle, rulesSubtitle);
         // Le conteneur est ajouté à la page
         add(container);
     }
@@ -58,6 +71,35 @@ public class MainView extends HtmlContainer {
             .set("background-image", "url(background.jpg)")
             .set("background-size", "cover")
             .set("background-repeat", "no-repeat");
+    }
+
+    public static VerticalLayout showRules(Dialog rules){
+
+        Button arrowLeftButton = new Button(new Icon(VaadinIcon.ARROW_LEFT));
+        Button arrowRightButton = new Button(new Icon(VaadinIcon.ARROW_RIGHT));
+        arrowRightButton.setIconAfterText(true);
+
+        HorizontalLayout arrowsLayout = new HorizontalLayout(arrowLeftButton, arrowRightButton);
+        arrowsLayout.setAlignItems(FlexComponent.Alignment.END);
+
+        VerticalLayout dialogLayout = new VerticalLayout(arrowsLayout);
+
+        arrowLeftButton.addClickListener(
+            click -> dialogLayout.getStyle()
+                .set("background-image", "url(rules/rules-page-1.png)"));
+
+        arrowRightButton.addClickListener(
+            click -> dialogLayout.getStyle()
+                .set("background-image", "url(rules/rules-page-2.png)"));
+
+        dialogLayout.getStyle()
+            .set("background-image", "url(rules/rules-page-1.png)")
+            .set("background-size", "contain")
+            .set("background-repeat", "no-repeat")
+            .set("width", "600px")
+            .set("height", "900px");
+
+        return dialogLayout;
     }
 
 }
