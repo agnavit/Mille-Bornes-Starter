252b9b019a2298f088d37b03ea580a29b3155405
Gnavit Anthony <68042249+agnavit@users.noreply.github.com>
2022-05-02 16:59:13 +1200

    Ajout commentaires

--- src/main/java/nc/unc/gl/borne/views/DeckLayout.java
+++ src/main/java/nc/unc/gl/borne/views/DeckLayout.java
@@ -4,10 +4,6 @@ import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import nc.unc.gl.borne.Deck.DeckService;
-import nc.unc.gl.borne.carte.Carte;
-import nc.unc.gl.borne.carte.NomCarte;
-import nc.unc.gl.borne.carte.TypeCarte;
 import nc.unc.gl.borne.joueur.Joueur;
 
 @StyleSheet("css/decklayout.css")
@@ -17,19 +13,26 @@ public class DeckLayout extends VerticalLayout {
 
         Image[] deckPlayerImage = new Image[player.getMain().getMainJoueur().size()];
 
+        //TODO: getSizeDeck() -> player.getMain().getMainJoueur().size()
         for(Integer i = 0; i < player.getMain().getMainJoueur().size(); i++){
+
+            //TODO: player.getCardImageName(i)
             deckPlayerImage[i] = new Image(
                 "cartes/"+player.getMain().getMainJoueur().get(i).getStringImage(),
                 player.getMain().getMainJoueur().get(i).getStringImage());
             deckPlayerImage[i].addClassName("image-deck");
+
+            var chosenCard = deckPlayerImage[i];
+            deckPlayerImage[i].addClickListener(
+                click -> {
+                    //click.getSource().getElement().getStyle().set("border-color", "gold");
+                    chosenCard.getElement().getStyle().set("border-color", "gold");
+                    System.out.println("Le joueur: " + player.getPseudo() + " a choisi la carte "+chosenCard.getAlt());
+                }
+            );
         }
 
         HorizontalLayout deckLayout = new HorizontalLayout(deckPlayerImage);
         add(deckLayout);
     }
-
-    //TODO
-    //    public void setDeckPlayer(Carte cartePiochee) {
-    //        this.deckPlayer[6] = cartePiochee;
-    //    }
 }
