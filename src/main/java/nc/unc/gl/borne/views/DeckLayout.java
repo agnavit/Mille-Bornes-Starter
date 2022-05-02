254822d9c16d6f2fce214ebebb2c2b0240856097
Gnavit Anthony <68042249+agnavit@users.noreply.github.com>
2022-05-03 11:02:21 +1200

    Maquette en plus

--- src/main/java/nc/unc/gl/borne/views/DeckLayout.java
+++ src/main/java/nc/unc/gl/borne/views/DeckLayout.java
@@ -4,30 +4,39 @@ import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import nc.unc.gl.borne.carte.Carte;
 import nc.unc.gl.borne.joueur.Joueur;
+import nc.unc.gl.borne.joueur.JoueurService;
 
 @StyleSheet("css/decklayout.css")
 public class DeckLayout extends VerticalLayout {
+
+    JoueurService playerService = new JoueurService();
 
     public DeckLayout(Joueur player){
 
         Image[] deckPlayerImage = new Image[player.getMain().getMainJoueur().size()];
 
-        //TODO: getSizeDeck() -> player.getMain().getMainJoueur().size()
-        for(Integer i = 0; i < player.getMain().getMainJoueur().size(); i++){
+        for(Integer i = 0; i < playerService.getSizeDeck(player); i++){
 
-            //TODO: player.getCardImageName(i)
             deckPlayerImage[i] = new Image(
-                "cartes/"+player.getMain().getMainJoueur().get(i).getStringImage(),
-                player.getMain().getMainJoueur().get(i).getStringImage());
+                "cartes/"+  playerService.getCardInDeck(player,i).getStringImage(),
+                String.valueOf(playerService.getCardInDeck(player,i)));
             deckPlayerImage[i].addClassName("image-deck");
 
+
             var chosenCard = deckPlayerImage[i];
+            int j = i;
             deckPlayerImage[i].addClickListener(
                 click -> {
                     //click.getSource().getElement().getStyle().set("border-color", "gold");
                     chosenCard.getElement().getStyle().set("border-color", "gold");
                     System.out.println("Le joueur: " + player.getPseudo() + " a choisi la carte "+chosenCard.getAlt());
+                    Carte selectedCard = new Carte(
+                        playerService.getCardInDeck(player,j).getNom(),
+                        playerService.getCardInDeck(player,j).getType(),
+                        playerService.getCardInDeck(player,j).getNumero()
+                    );
                 }
             );
         }
