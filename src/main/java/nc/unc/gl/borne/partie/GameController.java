2382113bad5d0a4716786072ee5e8a30fd666ec9
jmahoux <jasonmahoux@gmail.com>
2022-05-03 11:20:36 +1100

    test

--- src/main/java/nc/unc/gl/borne/partie/GameController.java
+++ src/main/java/nc/unc/gl/borne/partie/GameController.java
@@ -1,20 +1,14 @@
 package nc.unc.gl.borne.partie;
 
-import com.vaadin.flow.component.Text;
-import nc.unc.gl.borne.views.TestView;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/test")
 public class GameController {
 
     @GetMapping("/{idPartie}")
-    public String getIdPartie(@PathVariable String idPartie) {
-        TestView testView = new TestView();
-        testView.add(idPartie);
+    public String getIdPartie(@PathVariable("idPartie") String idPartie) {
         return idPartie;
     }
 }
