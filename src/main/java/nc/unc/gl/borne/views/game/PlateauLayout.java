d858b0d1b4c984c1df2549d44cb214ee53da8f90
Clementine Galin <galin.clem@gmail.com>
2022-05-13 20:51:35 +1100

    ajout getNbCartePileBorneString()

--- src/main/java/nc/unc/gl/borne/views/game/PlateauLayout.java
+++ src/main/java/nc/unc/gl/borne/views/game/PlateauLayout.java
@@ -52,41 +52,17 @@ public class PlateauLayout extends HorizontalLayout {
         HorizontalLayout layout = new HorizontalLayout();
 
         VerticalLayout pileVitesse = new VerticalLayout();
-
         pileVitesse.add(new H3("Vitesse"));
         getImageSommetPile(TypePile.VITESSE, joueur, pileVitesse);
-//        try {
-//            pileVitesse
-//                .add(new Image("cartes/" +
-//                    playerService.getLastCardInPile(joueur, TypePile.VITESSE).getStringImage(),
-//                    "carte/sommetPileVitesse"));
-//
-//        } catch(IndexOutOfBoundsException except){
-//            pileVitesse.add(new Image("cartes/back.png", "Dos de la carte"));
-//        }
 
         //------------------------------------------------------
-
 
         VerticalLayout pileBataille = new VerticalLayout();
         pileBataille.add(new H3("Bataille"));
 
         getImageSommetPile(TypePile.BATAILLE, joueur, pileBataille);
-//        try {
-//            pileBataille
-//                .add(new Image("cartes/" +playerService.getLastCardInPile(joueur,TypePile.BATAILLE)
-//                    .getStringImage(),
-//                    "carte/sommetPileBataille"));
-//
-//        } catch(IndexOutOfBoundsException except){
-//            pileBataille.add(new Image("cartes/back.png", "Dos de la carte"));
-//        }
-
-
-
 
         //------------------------------------------------------
-
 
         VerticalLayout pileBornes = new VerticalLayout();
         pileBornes.add(new H3("Bornes"));
@@ -95,53 +71,38 @@ public class PlateauLayout extends HorizontalLayout {
 
         Div vingtCinq = new Div();
         vingtCinq.add(new Image("cartes/borne_vingt_cinq.jpeg","cartes/borne_vingt_cinq"));
-        vingtCinq.add(new H4(String
-            .valueOf(joueur
-                .getPlateau()
-                .getPile(TypePile.BORNES)
-                .getNbCartePile(new Carte(NomCarte.VINGT_CINQ, TypeCarte.BORNE, 0)))));
+        vingtCinq.add(new H4(getNbCartePileBorneString(NomCarte.VINGT_CINQ, joueur)));
+
 
         Div cinquante = new Div();
         cinquante.add(new Image("cartes/borne_cinquante.jpeg","cartes/borne_cinquante"));
-        cinquante.add(new H4(String
-            .valueOf(joueur
-                .getPlateau()
-                .getPile(TypePile.BORNES)
-                .getNbCartePile(new Carte(NomCarte.CINQUANTE, TypeCarte.BORNE, 0)))));
+        cinquante.add(new H4(getNbCartePileBorneString(NomCarte.CINQUANTE, joueur)));
 
         Div soixanteQuinze = new Div();
         soixanteQuinze.add(new Image("cartes/borne_soixante_quinze.jpeg","cartes/borne_soixante_quinze"));
-        soixanteQuinze.add(new H4(String
-            .valueOf(joueur
-                .getPlateau()
-                .getPile(TypePile.BORNES)
-                .getNbCartePile(new Carte(NomCarte.SOIXANTE_QUINZE, TypeCarte.BORNE, 0)))));
+        soixanteQuinze.add(new H4(getNbCartePileBorneString(NomCarte.SOIXANTE_QUINZE, joueur)));
+
 
         Div cent = new Div();
         cent.add(new Image("cartes/borne_cent.jpeg","cartes/borne_cent"));
-        cent.add(new H4(String
-            .valueOf(joueur
-                .getPlateau()
-                .getPile(TypePile.BORNES)
-                .getNbCartePile(new Carte(NomCarte.CENT, TypeCarte.BORNE, 0)))));
+        cent.add(new H4(getNbCartePileBorneString(NomCarte.CENT, joueur)));
+
 
         Div deuxCents = new Div();
         deuxCents.add(new Image("cartes/borne_deux_cents.jpeg","cartes/borne_deux_cents"));
-        deuxCents.add(new H4(String
-            .valueOf(joueur
-                .getPlateau()
-                .getPile(TypePile.BORNES)
-                .getNbCartePile(new Carte(NomCarte.DEUX_CENTS, TypeCarte.BORNE, 0)))));
+        deuxCents.add(new H4(getNbCartePileBorneString(NomCarte.DEUX_CENTS, joueur)));
+
 
         pileBornesCartes.add(vingtCinq, cinquante, soixanteQuinze, cent, deuxCents);
 
         pileBornes.add(pileBornesCartes);
 
+        //------------------------------------------------------
 
         VerticalLayout pileBottes = new VerticalLayout();
         pileBottes.add(new H3("Pile bottes"));
 
-        if (joueur.getPlateau().getPile(TypePile.BOTTES) != null){
+        if (!joueur.getPlateau().getPile(TypePile.BOTTES).estVide()){
             for (int i = 0; i < playerService.getSizeDeck(joueur); i++) {
 
                 pileBottes.add(new Image("cartes/" + joueur
@@ -178,5 +139,12 @@ public class PlateauLayout extends HorizontalLayout {
             .add(new Image("cartes/" +playerService.getLastCardInPile(joueur,typePile)
                 .getStringImage(),
                 "carte/sommetPileBataille"));
+    }
+
+    public String getNbCartePileBorneString(NomCarte nomCarte, Joueur joueur){
+        return String.valueOf(joueur
+                .getPlateau()
+                .getPile(TypePile.BORNES)
+                .getNbCartePile(new Carte(nomCarte, TypeCarte.BORNE, 0)));
     }
 }
