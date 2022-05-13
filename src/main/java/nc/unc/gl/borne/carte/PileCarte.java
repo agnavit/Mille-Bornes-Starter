2e72d80fe8c315d5ae370ea959f012ba9b417e53
Clementine Galin <galin.clem@gmail.com>
2022-05-13 11:00:21 +1100

    PileCarte : ajout méthode estVide()

--- src/main/java/nc/unc/gl/borne/carte/PileCarte.java
+++ src/main/java/nc/unc/gl/borne/carte/PileCarte.java
@@ -105,4 +105,8 @@ public class PileCarte {
         PileCarte pileCarte1 = (PileCarte) o;
         return (Objects.equals(pileCarte, pileCarte1.pileCarte));
     }
+    
+    public boolean estVide(){
+        return pileCarte.size() == 0;
+    }
 }
