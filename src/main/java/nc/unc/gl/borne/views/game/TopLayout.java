3a1dd8d79c5976fa9bf221980b354ff9f1cae9c0
Gnavit Anthony <68042249+agnavit@users.noreply.github.com>
2022-05-16 17:23:42 +1200

    Optimisation top layout

--- src/main/java/nc/unc/gl/borne/views/game/TopLayout.java
+++ src/main/java/nc/unc/gl/borne/views/game/TopLayout.java
@@ -1,47 +1,43 @@
 package nc.unc.gl.borne.views.game;
 
-import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.Route;
 import nc.unc.gl.borne.metier.classes.Joueur;
 
-@Route("testTopLayout")
-@Tag("testTopLayout")
-@StyleSheet("css/toplayout.css")
 public class TopLayout extends VerticalLayout {
 
     private Joueur firstPlayer;
     private Joueur secondePlayer;
 
-    public TopLayout(Joueur myPlayer, Joueur notMyPlayer){
+    public TopLayout(Joueur me, Joueur notMe) {
 
         VerticalLayout topLayout = new VerticalLayout();
 
-        if(myPlayer.getScore() == notMyPlayer.getScore()){
-            H2 highScore = new H2("1er " + myPlayer.getPseudo() + " : " + myPlayer.getScore());
+        if(me.getScore() == notMe.getScore()) {
+
+            H2 highScore = new H2("1er " + me.getPseudo() + " : " + me.getScore());
             highScore.addClassName("first-player");
 
-            H2 lowScore = new H2("1er " + notMyPlayer.getPseudo() + " : " + notMyPlayer.getScore());
+            H2 lowScore = new H2("1er " + notMe.getPseudo() + " : " + notMe.getScore());
             lowScore.addClassName("first-player");
+
             topLayout.add(highScore, lowScore);
             topLayout.addClassName("top");
 
         } else {
 
-            if(myPlayer.getScore() > notMyPlayer.getScore()){
-                firstPlayer = myPlayer;
-                secondePlayer = notMyPlayer;
-            } else if(myPlayer.getScore() < notMyPlayer.getScore()){
-                firstPlayer = notMyPlayer;
-                secondePlayer = myPlayer;
+            if(me.getScore() > notMe.getScore()) {
+                firstPlayer = me;
+                secondePlayer = notMe;
+            } else if(me.getScore() < notMe.getScore()) {
+                firstPlayer = notMe;
+                secondePlayer = me;
             }
 
             H2 highScore = new H2("1er " + firstPlayer.getPseudo() + " : " + firstPlayer.getScore());
             highScore.addClassName("first-player");
 
-            H2 lowScore = new H2("2nd " +secondePlayer.getPseudo() + " : " + secondePlayer.getScore());
+            H2 lowScore = new H2("2nd " + secondePlayer.getPseudo() + " : " + secondePlayer.getScore());
             lowScore.addClassName("seconde-player");
 
             topLayout.add(highScore, lowScore);
